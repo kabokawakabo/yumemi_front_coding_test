f70a3940073724528e5d6f21b1a5f7aaa482adb8
kabokawakabo <murakami@murakaminoMacBook-ea.local>
2023-04-22 16:42:19 +0900

    trivial: storybook上のcheckboxファイル構造名をAtomに変更

--- src/uiParts/Checkbox/index.stories.ts
+++ src/uiParts/Checkbox/index.stories.ts
@@ -7,7 +7,7 @@ import { Checkbox } from ".";
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
 const meta = {
-  title: "Atomic/Checbox",
+  title: "Atom/Checbox",
   component: Checkbox,
   tags: ["autodocs"],
 } satisfies Meta<typeof Checkbox>;
