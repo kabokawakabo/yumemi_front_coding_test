3f3c753e0f275b0ee4c8c0aa8ae8c4eb7df6b27a
kabokawakabo <murakami@murakaminoMacBook-ea.local>
2023-04-22 15:56:31 +0900

    update: checkboxのプロパティ：className追加
    checkboxList（親コンポーネント）からスタイル指定するため

--- src/uiParts/Checkbox/index.tsx
+++ src/uiParts/Checkbox/index.tsx
@@ -1,9 +1,11 @@
 type CheckboxProps = {
+  className?: string;
   label: string;
   value: boolean;
   setValue: (next_value: boolean) => void;
 };
 export const Checkbox: React.FC<CheckboxProps> = ({
+  className,
   label,
   value,
   setValue,
@@ -13,7 +15,7 @@ export const Checkbox: React.FC<CheckboxProps> = ({
   };
 
   return (
-    <label>
+    <label className={className}>
       <input type="checkbox" checked={value} onChange={onChange} /> {label}
     </label>
   );
