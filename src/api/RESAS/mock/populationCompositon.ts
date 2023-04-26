565e60f0d037cfcc223b85a197d4f1622ce48f26
kabokawakabo <murakami@wlan-napt-002.u-aizu.ac.jp>
2023-04-26 18:14:13 +0900

    fix: 人口構成モックデータ最大数計算修正
    排他的論理和になっていたため、指数計算に変更

--- src/api/RESAS/mock/populationCompositon.ts
+++ src/api/RESAS/mock/populationCompositon.ts
@@ -3,7 +3,7 @@ import type {
   PopulationCompositionPerYear,
 } from "../populationComposition";
 
-const MAX_POPULATION = 10 ^ 9; // NOTE: 東京の2045年は8桁代だったため、この値が入るように
+const MAX_POPULATION = 10 ** 9; // NOTE: 東京の2045年は8桁代だったため、この値が入るように
 const DECIMAL_PLACE_RATE = 100;
 export const LABELS = ["総人口", "年少人口", "生産年齢人口", "老年人口"];
 
