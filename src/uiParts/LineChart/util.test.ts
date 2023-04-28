f8fef5bf5f6161b2690055b792940ced0fdb196f
kabokawakabo <murakami@murakaminoMacBook-ea.local>
2023-04-28 14:09:33 +0900

    update: LineChart| tickフォーマット用関数追加
    単位を追加する関数、単位&有効数字までしか表示しない関数 の二つ作成

--- src/uiParts/LineChart/util.test.ts
+++ src/uiParts/LineChart/util.test.ts
@@ -1,4 +1,8 @@
-import { jaTickFormatter } from "./util";
+import {
+  jaTickFormatter,
+  addRankSuffix,
+  addRankSuffixWithSignificantDigit,
+} from "./util";
 
 describe("LineChart ui", () => {
   test("jaTickFormatter| 単位表示変更チェック", () => {
@@ -16,5 +20,34 @@ describe("LineChart ui", () => {
     expect(jaTickFormatter(1000, true)).toBe("1000");
     expect(jaTickFormatter(10000100, true)).toBe("1000万");
     expect(jaTickFormatter(1000000111111, true)).toBe("10000億");
+  });
+
+  test("addRankSuffix| 単位が追加された文字列を返すか", () => {
+    expect(addRankSuffix(10 ** 3)).toBe("1000");
+    expect(addRankSuffix(10 ** 4)).toBe("1万");
+    expect(addRankSuffix(10 ** 7)).toBe("1000万");
+    expect(addRankSuffix(10 ** 8)).toBe("1億");
+    expect(addRankSuffix(10 ** 13)).toBe("10兆");
+    expect(addRankSuffix(10 ** 16)).toBe("10000兆");
+
+    expect(addRankSuffix(10 ** 9 + 10 ** 2)).toBe("10億100");
+    expect(addRankSuffix(10 ** 16 + 10 ** 5)).toBe("10000兆10万");
+  });
+
+  test("addRankSuffixWithSignificantDigit| 最大単位 & 有効数字まで表示", () => {
+    expect(addRankSuffixWithSignificantDigit(10 ** 7 + 10 ** 5, 2)).toBe(
+      "1010万"
+    );
+    expect(addRankSuffixWithSignificantDigit(10 ** 9 + 10 ** 6, 5)).toBe(
+      "10.010億"
+    );
+    expect(addRankSuffixWithSignificantDigit(10 ** 9 + 10 ** 2, 3)).toBe(
+      "10.0億"
+    );
+
+    expect(addRankSuffixWithSignificantDigit(10 ** 8 + 10 ** 4, 5)).toBe(
+      "1.0001億"
+    );
+    expect(addRankSuffixWithSignificantDigit(10 ** 9, 0)).toBe("10億");
   });
 });
--- src/uiParts/LineChart/util.ts
+++ src/uiParts/LineChart/util.ts
@@ -43,3 +43,90 @@ export const jaTickFormatter = (num_label: number, forceFormat?: boolean) => {
   );
   return ten_thousand_label; /// NOTE: 万単位に足りない場合、関数内部では変更しないのでそのまま返せる
 };
+
+const UNIT_RANK = 4;
+const UNIT_SUFFIX_LIST = ["", "万", "億", "兆"];
+
+type Unit = {
+  value: string;
+  suffix: string;
+  isLastOrOverSuffix: boolean;
+};
+
+export const createUnitList = (label: string) => {
+  const len = label.length;
+  const unit_list: Unit[] = [];
+
+  for (let i = 0, suf_i = 0; i < len; i += UNIT_RANK, suf_i++) {
+    const ed_i = len - i;
+    const value = label.substring(ed_i - UNIT_RANK, ed_i);
+    const suffix = UNIT_SUFFIX_LIST[suf_i] ?? ""; // 該当する値がない場合単位を加えない
+
+    unit_list.push({
+      value,
+      suffix,
+      isLastOrOverSuffix: suf_i >= UNIT_SUFFIX_LIST.length - 1,
+    });
+  }
+
+  return unit_list;
+};
+
+const isAllZeroStr = (str: string) => str.split("").every((d) => d === "0");
+
+export const addRankSuffix = (label_num: number) => {
+  const label = label_num + "";
+  const unit_list = createUnitList(label);
+
+  let new_label = "";
+  for (const { value, suffix, isLastOrOverSuffix } of unit_list) {
+    if (isLastOrOverSuffix) {
+      /// NOTE: 1 0000万（億単位なし） の場合、0000部分を加える。
+      new_label = value + suffix + new_label;
+    } else if (!isAllZeroStr(value)) {
+      /// NOTE: 0100 の場合、 Number関数で 100に変更。
+      new_label = Number(value) + suffix + new_label;
+    }
+  }
+
+  return new_label;
+};
+
+export const addRankSuffixWithSignificantDigit = (
+  label_num: number,
+  tol_digit: number
+) => {
+  const label = label_num + "";
+  const unit_list = createUnitList(label);
+  const reverse_unit_list = [...unit_list].reverse();
+
+  let digit_count = 0;
+  let new_label = "";
+  let int_suffix = "";
+  let hasNotDecimalPoint = true;
+  for (const i of reverse_unit_list.keys()) {
+    const { value, suffix, isLastOrOverSuffix } = reverse_unit_list[i];
+    // NOTE: 初回 or 単位がつくまでは加え続ける
+    if (i === 0 || isLastOrOverSuffix) {
+      new_label += value;
+      digit_count = value.length;
+      int_suffix = suffix;
+      continue;
+    }
+
+    if (digit_count >= tol_digit) break;
+
+    // 単位以降で桁が足りない場合は追記
+    if (hasNotDecimalPoint) {
+      new_label += ".";
+      hasNotDecimalPoint = false;
+    }
+    for (const v of value) {
+      new_label += v;
+      digit_count++;
+      if (digit_count >= tol_digit) break;
+    }
+  }
+
+  return new_label + int_suffix;
+};
