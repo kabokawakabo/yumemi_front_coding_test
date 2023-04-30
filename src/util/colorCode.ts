1e83491b1190edba2586660fc422f3c2d412cba8
kabokawakabo <murakami@murakaminoMacBook-ea.local>
2023-04-30 15:08:10 +0900

    update: util| colorCodeバリデーション、エラー文を具体的に書くように変更

--- src/util/colorCode.ts
+++ src/util/colorCode.ts
@@ -18,10 +18,16 @@ export const validateInPicker = (color_code: string) => {
   // NOTE:  color_pickerの valueは #f00 形式では反映されないため形式チェック
   const isNotLen7 = color_code.length !== 7;
   const hasNotShape = color_code[0] !== "#";
-  if (isNotLen7 || hasNotShape) throw new ColorCodeInPickerError(color_code);
+  if (isNotLen7 || hasNotShape)
+    throw new ColorCodeInPickerError(
+      `${color_code}: inputでは処理できないカラーコード長、または #がありません。`
+    );
 
   const hex_str = color_code.substring(1);
   const hex = parseInt(hex_str, RADIX);
   const isNotHex = Number.isNaN(hex);
-  if (isNotHex) throw new ColorCodeInPickerError(color_code);
+  if (isNotHex)
+    throw new ColorCodeInPickerError(
+      `${color_code}: #以下が16進数ではありません。`
+    );
 };
