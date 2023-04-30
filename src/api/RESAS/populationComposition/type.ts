435e816fd20378bed60eec87b0dbe5bdd5795ca1
kabokawakabo <murakami@murakaminoMacBook-ea.local>
2023-05-01 01:12:00 +0900

    trivial: 不要なコメントアウト削除。コメント追記

--- src/api/RESAS/populationComposition/type.ts
+++ src/api/RESAS/populationComposition/type.ts
@@ -10,6 +10,6 @@ export type PopulationCompositionPerYear = {
 
 export type getPopulationPerYearProps = {
   prefCode: number;
-  cityCode?: number; // undefinedで「すべての市区町村」を選択
+  cityCode?: number; // undefinedで「すべての市区町村」を選択する文字列に内部で変更
   addArea?: string;
 };
--- src/uiParts/LegendColorPickerList/index.stories.ts
+++ src/uiParts/LegendColorPickerList/index.stories.ts
@@ -6,7 +6,6 @@ import { expect } from "@storybook/jest";
 import { LegendColorPickerList } from ".";
 import style from "./style.module.css";
 import { LegendColorPickerArg } from "./setting_stories";
-//import { LABELS, CheckboxListArgs, hasLabel } from "./setting_stories";
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
 const meta = {
